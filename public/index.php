15c3983b0550b8ddca5a04e638273583bc9df272
Abdullahi Kabri <kabriacid01@gmail.com>
2024-12-20 15:30:57 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<?php
+require("../includes/layout/header.php");
+?>
+
+
 <body>
-   <h1>HELLO WORLD</h1> 
+    <h1>HELLO WORLD</h1>
 </body>
-</html>
+
+<?php
+require("../includes/layout/footer.php");
+?>
