9261ff21f4e4e687924e05a961d311ad3c005b8e
Sirbarkindo <sirbarkindo515@gmail.com>
2024-12-20 15:29:07 +0100

    comit

--- public/index.php
+++ public/index.php
@@ -2,10 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-   <h1>HELLO WORLD</h1> 
+    <h1>Sign Up</h1>
+    <form action="#" method="post">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="confirm-password">Confirm Password:</label><br>
+        <input type="password" id="confirm-password" name="confirm-password" required><br><br>
+
+        <input type="submit" value="Sign Up">
+    </form>
 </body>
 </html>
